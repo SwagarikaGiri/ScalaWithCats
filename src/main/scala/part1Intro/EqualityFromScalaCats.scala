b5ce5ff4e5381f9264a4c05d1c53e5f9ae009de4
SwagarikaGiri <swagaarika95@gmail.com>
2020-06-10 12:46:12 +0530

    Cat example with cats

--- src/main/scala/part1Intro/EqualityFromScalaCats.scala
+++ src/main/scala/part1Intro/EqualityFromScalaCats.scala
@@ -1,5 +1,15 @@
 package part1Intro
+import cats.Eq
+import cats.instances.int._
 
-object EqualityFromScalaCats {
+object EqualityFromScalaCats extends App{
 
+
+
+  implicit val intInstance = Eq[Int]
+  println(intInstance.eqv(89,90))
+
+  import cats.syntax.eq._ // for commands === !=
+  println(123 === 123)
+  println(123 =!= 344)
 }
--- src/main/scala/part1Intro/OptionTypeInScala.scala
+++ src/main/scala/part1Intro/OptionTypeInScala.scala
@@ -1,3 +1,26 @@
 package part1Intro
+/*
+ Option[T] is a container that return Some(T) or None
+ */
+object OptionTypeInScala extends App{
 
-case class OptionTypeInScala()
+  val capitals = Map("France" -> "Paris", "Japan" -> "Tokyo")
+
+  println("capitals.get( \"France\" ) : " +  capitals.get( "France" ))
+  println("capitals.get( \"India\" ) : " +  capitals.get( "India" ))
+
+  def show(x: Option[String]) = x match {
+    case Some(s) => s
+    case None => "?"
+  }
+  println("capitals.get( \"France\" ) : " +  show(capitals.get( "France" )))
+  println("capitals.get( \"India\" ) : " +  show(capitals.get( "India" )))
+
+
+  val a:Option[Int] = Some(5)
+  val b:Option[Int] = None
+
+  println("a.isEmpty: " + a.isEmpty )
+  println("b.isEmpty: " + b.isEmpty )
+}
+
