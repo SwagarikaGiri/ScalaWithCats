f92f20d144b28b33ef903131c15fe49ac35cb8cd
SwagarikaGiri <swagaarika95@gmail.com>
2020-06-14 23:57:00 +0530

    Semigroupal intro

--- src/main/scala/part6SemiGroupalApplicative/Intro.scala
+++ src/main/scala/part6SemiGroupalApplicative/Intro.scala
@@ -1,5 +1,26 @@
 package part6SemiGroupalApplicative
 
-object Intro {
+import cats.syntax.either._ // for catchOnly
+
+
+object Intro extends App{
+  /*
+  In monad and Functor we are able sequence operations/computations
+  but in case we fail in first example it breaks
+  in cases like form we need to return all the error at once
+  flatMap aren’t quite capable of capturing what we want because they make the assump􏰁on that each computa􏰁on is dependent on the previous
+   */
+  def parseInt(str: String): Either[String, Int] = Either.catchOnly[NumberFormatException](str.toInt).
+    leftMap(_ => s"Couldn't read $str")
+  val res1 = for {
+    a <- parseInt("a")
+    b <- parseInt("b")
+    c <- parseInt("c")
+  } yield (a + b + c)
+  println(res1)
+  // res1: scala.util.Either[String,Int] = Left(Couldn't read a)
+  /*
+  so only one error was returned
+   */
 
 }
--- src/main/scala/part6SemiGroupalApplicative/SemiGroupal.scala
+++ src/main/scala/part6SemiGroupalApplicative/SemiGroupal.scala
@@ -1,5 +1,35 @@
 package part6SemiGroupalApplicative
+import cats.Semigroupal
+import cats.instances.option._ //Semigroupal
+import cats.syntax.apply._
 
-object SemiGroupal {
+/*
+   Functor or monad construct makes an assumption that the current computation/function is dependent on the previous one
+   So there comes SemiGroupal that extends Functor and Semigroup
+   trait Semigroupal[F[_]]{
+   def product[A,B](fa: F[A], fb: F[B]): F[(A, B)]
+   }
 
+ */
+object SemiGroupal extends App{
+
+
+  val res= Semigroupal[Option].product(Some(123), Some("abc"))
+  println(res)
+  val res1= Semigroupal[Option].product(Some(123),None)
+  println(res1)
+  // joinning three of more context
+  val res2= Semigroupal.tuple3(Option(10),Option(20),Option(30))
+  println(res2)
+  val res3 =Semigroupal.map3(Option(1), Option(2), Option(3))(_ + _ + _)
+  println(res3)
+
+  println(Semigroupal.map2(Option(19), Option(0))(_ + _))
+  // res6: Option[Int] = None
+
+  /*
+      There has also been beautiful syntax
+   */
+  
+  println((Option(7),Option(7),Option(10)).tupled)
 }
